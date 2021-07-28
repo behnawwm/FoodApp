c94c4e94fc4686454ba6cc531dba7514bea935e0
Behnam Banaei <61078796+behnawwm@users.noreply.github.com>
2021-07-28 21:54:09 +0430

    more fragment

--- app/src/main/java/com/example/foodapp/ui/Home/fragments/MoreFragment.kt
+++ app/src/main/java/com/example/foodapp/ui/Home/fragments/MoreFragment.kt
@@ -6,8 +6,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.foodapp.R
+import com.example.foodapp.databinding.FragmentMoreBinding
+import com.example.foodapp.databinding.FragmentProfileBinding
+import com.github.dhaval2404.imagepicker.ImagePicker
 
 
 class MoreFragment : Fragment(R.layout.fragment_more) {
+    lateinit var binding: FragmentMoreBinding
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMoreBinding.inflate(inflater)
+
+        binding.apply {
+
+
+
+        }
+
+        return binding.root
+    }
 }

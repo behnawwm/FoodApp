55d802decfb6cd9352e7190dc56e1cd2d7fd2f6b
Behnam Banaei <61078796+behnawwm@users.noreply.github.com>
2021-07-28 15:30:19 +0430

    profile fragment

--- app/src/main/java/com/example/foodapp/ui/Home/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/foodapp/ui/Home/fragments/ProfileFragment.kt
@@ -1,13 +1,55 @@
 package com.example.foodapp.ui.Home.fragments
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.foodapp.R
+import com.example.foodapp.databinding.FragmentProfileBinding
+import com.github.dhaval2404.imagepicker.ImagePicker
 
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
+    lateinit var binding: FragmentProfileBinding
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentProfileBinding.inflate(inflater)
+
+        binding.apply {
+            fabChangeProfile.setOnClickListener {
+                ImagePicker.with(this@ProfileFragment)
+                    .crop()                    //Crop image(Optional), Check Customization for more option
+                    .start()
+            }
+
+
+        }
+
+        return binding.root
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            //Image Uri will not be null for RESULT_OK
+            val uri: Uri = data?.data!!
+
+            // Use Uri object instead of File to avoid storage permissions
+            binding.ivProfile.setImageURI(uri)
+        } else if (resultCode == ImagePicker.RESULT_ERROR) {
+            Toast.makeText(context, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(context, "Task Cancelled", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
